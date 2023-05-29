b97f6d331c5ca7235baea7582f986280215b1c8e
Princessa <princessafusanto@gmail.com>
2023-05-29 14:10:34 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 // Route::get('/cekorder', function () {
 //     return view('cekorder');
 // });
